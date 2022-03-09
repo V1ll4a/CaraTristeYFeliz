0bcdc00fda56e5add44315a95a4fc1cd0a7711e9
V1ll4a <92698844+V1ll4a@users.noreply.github.com>
2022-03-08 22:18:20 -0500

    Update condicionales3.js

--- condicionales3.js
+++ condicionales3.js
@@ -2,14 +2,14 @@ let urlcarafeliz = "https://images.emojiterra.com/google/android-10/512px/1f60a.
 let urlcaratriste = "https://cdn-0.emojis.wiki/emoji-pics/facebook/pensive-face-facebook.png"
 let imgHtml = document.querySelector("#imagencara")
 do{
-    nota1 = parseInt(prompt("ingrese su nota "))
-} while (isNaN (nota1))
+    nota1 = parseInt(prompt("ingrese su primera nota "))
+} while (isNaN (nota1)|| nota1<0 || nota1>10)
 do{
-    nota2 = parseInt(prompt("ingrese su nota "))
-} while (isNaN (nota2))
+    nota2 = parseInt(prompt("ingrese su segunda nota "))
+} while (isNaN (nota2) || nota2<0 || nota3>10)
 do{
-    nota3 = parseInt(prompt("ingrese su nota "))
-} while (isNaN (nota3))
+    nota3 = parseInt(prompt("ingrese su tercera nota "))
+} while (isNaN (nota3) || nota3<0 || nota3>10)
 let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
 alert("Su promedio es: "+ p)
 if (p>6){
